fa27707b9f50ab2fd6f7b7d22c0429fbf7ddf384
mys1erious <subtletyzeros@gmail.com>
2024-02-12 00:49:31 +0100

    feat: add first v. of header and hanging sign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import Header from "./components/Header";
 import Image from "next/image";
+import React from "react";
 
+
+// 'rsc' for components
 export default function Home() {
     return (
-        <main>
-            <div>.</div>
+        <main className="main-bg min-h-screen">
+            {/* TODO: fix image pixeling on hover wtf */}
+            <div className="hanging-sign-container">
+                <Image className="hanging-sign"
+                       src="/mys1erious_sign.png" alt="mys1erious sign"
+                       width={200} height={100}/>
+            </div>
+            <div className="flex flex-col">
+                <Header/>
+                <div>main section</div>
+                <footer>footer</footer>
+            </div>
         </main>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                darkblue: '#0b1823',
+                lightblue: '#132b3f',
+                deepblack: '#0a0f0d',
+            },
+            boxShadow: {
+                'light': '0 0 4px 2px rgba(255, 255, 255, 0.2)'
+            }
+        },
+    },
+    plugins: [],
 }
-
