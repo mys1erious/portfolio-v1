415366ff876f1cecb811e9a5a20b5b91a2a10368
mys1erious <subtletyzeros@gmail.com>
2024-03-24 02:39:07 +0100

    feat: add portal, all current skill icons, basic responsive layout

--- src/app/components/Icon.tsx
+++ src/app/components/Icon.tsx
@@ -28,8 +28,8 @@ const Icon = ({href="", src, alt, width = 24, height = 24, text}: IconProps) => 
                 <Image className="transition-transform duration-500 ease-in-out hover:scale-125"
                        src={src} alt={alt} width={width} height={height}
                 />
-                <div className={`bottom-full absolute opacity-0 pointer-events-none left-1/2 -translate-x-1/2 
-                     transition-opacity duration-500 ease-in-out group-hover:opacity-100`}>
+                <div className="bottom-full absolute opacity-0 pointer-events-none left-1/2 -translate-x-1/2
+                     transition-opacity duration-500 ease-in-out group-hover:opacity-100">
                     <div className="px-3 py-1 bg-black text-sm rounded-3xl whitespace-nowrap">{text}</div>
                     <div className="mt-[-8px] text-gray-600">&#9660;</div>
                 </div>
